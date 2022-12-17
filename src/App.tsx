1256ceccf89e20d7064b3da9ac22bb6173f1984f
Mirtineh <mirfil92@gmail.com>
2022-12-17 12:34:34 +0300

    first personal inflo phase

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import Stage from "./components/Stage";
 function App() {
   return (
     <>
-      <div className="flex flex-col sm:flex-row sm:p-4 h-screen relative">
+      <div className="flex flex-col sm:flex-row sm:p-4 h-screen">
         <div className="basis-1/3 sm:basis-3/12">
           <Stage />
         </div>
-        <div>
-          <PersonalInfo />
+        <div className="bg-magnolia sm:bg-white h-full w-full relative">
+          <div className="absolute sm:static w-72 h-full sm:w-[500px] mx-auto p-7  my-auto rounded-md -top-16 bg-white">
+            <PersonalInfo />
+          </div>
         </div>
       </div>
     </>
