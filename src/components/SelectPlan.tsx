e16b0711219b2725d8d0e2adf87cc07e67614f11
Mirtineh <mirfil92@gmail.com>
2022-12-19 16:53:36 +0300

    add fontawesome dependency

--- src/components/SelectPlan.tsx
+++ src/components/SelectPlan.tsx
@@ -1,7 +1,8 @@
 import { FunctionComponent } from "react";
 import Content from "./Content";
-import iconArcade from "../assets/images/icon-arcade.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Plan from "./Plan";
+import { faToggleOn } from "@fortawesome/free-solid-svg-icons";
 
 interface SelectPlanProps {}
 
@@ -16,8 +17,9 @@ const SelectPlan: FunctionComponent<SelectPlanProps> = () => {
           <Plan type="arcade" isActive={true} />
           <Plan type="advanced" isActive={false} />
           <Plan type="pro" isActive={false} />
-          <div className="flex justify-center">
+          <div className="flex justify-center items-center gap-6 py-3 bg-pastel-blue bg-opacity-10 rounded-lg mt-3">
             <span>Monthly</span>
+            <FontAwesomeIcon icon={faToggleOn} />
             <span>Yearly</span>
           </div>
         </div>
